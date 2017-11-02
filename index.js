bc7b4f748ed2d414d49e51bab6a37e4c64504c2a
rosy <edoardolenzi9@gmail.com>
2017-11-02 22:00:12 +0100

    hello world heroku

--- index.js
+++ index.js
@@ -1,104 +1,19 @@
-/*globals require, console, process */
+//Libraries
+var http = require('http');
 
-//express
-var express = require('express');
-var bodyParser = require('body-parser');
-//inspect variables
-var util = require('util');
-
-//instantiate express
-var app = express();
-const hostname = '127.0.0.1';
-const port = (process.env.PORT || 8090);
-
-/* Configure express app to use bodyParser()
- * to parse body as URL encoded data
- * (this is how browser POST form data)
- */
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-//handle get req on /sum
-app.get('/sum', function (req, res) {    
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    //process
-    var x = parseFloat(req.query.x);
-    var y = parseFloat(req.query.y);
-    var sum = x + y;
-    
-    //write response
-    res.write('?x: ' + x + '\n');
-    res.write('?y: ' + y + '\n');
-    res.write('sum: ' + sum + '\n');
-    //send response
-    res.end();
-});
-
-
-//handle get req on /sum
-app.get('/multiply', function (req, res) {    
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    //process
-    var x = parseFloat(req.query.x);
-    var y = parseFloat(req.query.y);
-    var multiply = x * y;
-    
-    //write response
-    res.write('?x: ' + x + '\n');
-    res.write('?y: ' + y + '\n');
-    res.write('multiply: ' + multiply + '\n');
-    //send response
-    res.end();
-});
-
-
-//handle post req on /
-app.post('/sum', function (req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    //process
-    var x = parseFloat(req.body.x);
-    var y = parseFloat(req.body.y);
-    var sum = x + y;
-    
-    //write response
-    res.write('?x: ' + x + '\n');
-    res.write('?y: ' + y + '\n');
-    res.write('sum: ' + sum + '\n');
-    //send response
-    res.end();
-    
-});
-
-
-//handle post req on /
-app.post('/multiply', function (req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    //process
-    var x = parseFloat(req.body.x);
-    var y = parseFloat(req.body.y);
-    var multiply = x * y;
-    
-    //write response
-    res.write('?x: ' + x + '\n');
-    res.write('?y: ' + y + '\n');
-    res.write('multiply: ' + multiply + '\n');
-    //send response
-    res.end();
-    
-});
-
+//create a server
+var server = http.createServer(
+   function (req, res) {
+       //HTML head (type of the page)
+       res.writeHead(200, {'Content-Type': 'text/plain'});
+       
+       //HTML content
+       res.end('Hello World');
+   }
+);
 
 //listen in a specific port
-app.listen(port, hostname);
+server.listen(process.env.PORT || 80);
 
 //check status
-console.log('Server running at http://localhost:80/');
+console.log('Server listening on port '+process.env.PORT);
